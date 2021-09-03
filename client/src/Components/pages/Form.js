3d06ec73c6d15bb9eaebd297d508dce8850a7772
Damian Thompasionas <dthompasionas@gmail.com>
2021-09-02 20:23:01 -0400

    added signup validation

--- client/src/Components/pages/Form.js
+++ client/src/Components/pages/Form.js
@@ -5,6 +5,10 @@ import '../pages/Form.css';
 
 function Form() {
 
+    const [fullNameReg, setFullNameReg] = useState('')
+    const [emailReg, setEmailReg] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
+
     return (
         <div className='Form'>
             <div className='form-container'>
@@ -12,15 +16,21 @@ function Form() {
                     <h1>Sign up</h1>
                     <div className='form-input'>
                         <label htmlFor='name' className='form-label'>Full Name:</label>
-                        <input type='text' name='name' placeholder='Enter full name'/>
+                        <input type='text' name='name' placeholder='Enter full name' onChange={(e) => {
+                            setFullNameReg(e.target.value);
+                        }}/>
                     </div>
                     <div className='form-input'>
                         <label htmlFor='email' className='form-label'>Email:</label>
-                        <input type='email' name='email' placeholder='Enter your email'/>
+                        <input type='email' name='email' placeholder='Enter your email' onChange={(e) => {
+                            setEmailReg(e.target.value);
+                        }}/>
                     </div>
                     <div className='form-input'>
                         <label htmlFor='password' className='form-label'>Password:</label>
-                        <input type='password' name='password' placeholder='Enter A Password'/>
+                        <input type='password' name='password' placeholder='Enter A Password' onChange={(e) => {
+                            setPasswordReg(e.target.value);
+                        }}/>
                     </div>
                     <button className='form-btn'>Sign up</button>           
                 </div>

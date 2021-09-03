0698b3148824646fb1a86ed32c2980204822d647
mcfulmer13 <macfulmer@live.com>
2021-09-02 22:46:52 -0400

    image link

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import HomePage from './Components/HomePage/HomePage';
-=======
 
 import UserProfile from './Components/UserProfile/UserProfile';
 
->>>>>>> ecd80a697a452feda1ef10ac09d1cd536dff33b7
 import Navbar from './Components/Navbar/Navbar';
 import Form from './Components/pages/Form';
 import JobSearch from './Components/JobSearch/JobSearch';
@@ -22,12 +18,8 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-<<<<<<< HEAD
-          <Route path='/' exact component={HomePage}/>
-=======
           <UserProfile/>
           <Route path='/' exact />
->>>>>>> ecd80a697a452feda1ef10ac09d1cd536dff33b7
           <Route path='/Form' component={Form} />
           <Route path='/HomePage' component={HomePage}/>
           <Route path='/JobSearch' component={JobSearch}/>
